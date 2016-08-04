e6e656f362885a18182e8777ea646ac21c8c646c
facundoq <facundoq>
2016-08-03 22:17:00 -0300

    Started implementing parser

--- src/main/scala/vonemu/lang/Language.scala
+++ src/main/scala/vonemu/lang/Language.scala
@@ -2,40 +2,74 @@ package vonemu.lang
 
 import enumeratum._
 
-sealed trait Register extends EnumEntry
-
-object Register extends Enum[Register] {
- 
-  case object AX extends Register
-  case object BX extends Register
-  case object CX extends Register
-  case object DX extends Register
-  case object AH extends Register
-  case object BH extends Register
-  case object CH extends Register
-  case object DH extends Register
-  case object AL extends Register
-  case object BL extends Register
-  case object CL extends Register
-  case object DL extends Register
-  val values = findValues
- 
-}
 
 
-object Language {
 
-  
-  type MemoryAdress = Int
-   sealed abstract class Instruction(val line:Int) {
-
+  object RegisterID extends Enumeration {
+    type RegisterID = Value
+    val a, b, c, d = Value
   }
 
-  case class AddRR(override val line:Int,val a: Register,val b: Register) extends Instruction(line)
-  case class SubRR(override val line:Int,val a: Register,val b: Register) extends Instruction(line)
-  case class AddRM(override val line:Int,val a: Register,val b: MemoryAdress) extends Instruction(line)
-  case class SubRM(override val line:Int,val a: Register,val b: MemoryAdress) extends Instruction(line)
-  case class AddMR(override val line:Int,val a: MemoryAdress,val b: Register) extends Instruction(line)
-  case class SubMR(override val line:Int,val a: MemoryAdress,val b: Register) extends Instruction(line)
+  object RegisterPart extends Enumeration {
+     type RegisterPart = Value
+    val L, H, X = Value // low high all
+  }
+  import RegisterID._
+  import RegisterPart._
+   
+  sealed abstract class Operand
+
+  case class Register(id: RegisterID, part: RegisterPart) extends Operand
+  case class MemoryAdress(adress:Int) extends Operand  
   
+  trait Operator
+  trait ZeroaryOperator extends Operator
+  case object Ret extends ZeroaryOperator
+  case object Hlt extends ZeroaryOperator
+  case object End extends ZeroaryOperator
+ 
+  trait UnaryOperator extends Operator
+  case object Org extends UnaryOperator
+  case object Not extends UnaryOperator
+  case object Jmp extends UnaryOperator
+
+ trait BinaryOperator extends Operator
+  case object Add extends BinaryOperator
+  case object Sub extends BinaryOperator
+  case object Mov extends BinaryOperator
+
+  abstract class Instruction(val line: Int) 
+  class BinaryInstruction(line:Int,val o:BinaryOperator,val a:Operand,val b:Operand) extends Instruction(line)
+  class UnaryInstruction(line:Int,val o:UnaryOperator,val a:Operand) extends Instruction(line)
+  class ZeroaryInstruction(line:Int,val o:ZeroaryOperator) extends Instruction(line)
+  
+
+  class Line(line: Int, tokens: Array[String])
+
+  class ParseError(val line: Int, val reason: String)
+class Parser{
+  def parse(code: String): Either[Array[ParseError], Array[Instruction]] = {
+    parse(code.split('\n'))
+  }
+  def parse(code: Array[String]): Either[Array[ParseError], Array[Instruction]] = {
+    val tokensPerLine = code.map(line => line.split(' '))
+    val lines = tokensPerLine.zipWithIndex.map { case (tokens, line) => new Line(line, tokens) }
+    val parsedLines = lines.map(parseLine)
+    val errors = parsedLines.filter(p => p.isLeft).map(_.left.get)
+    if (!errors.isEmpty) {
+      return Left(errors)
+    }
+    val correctlyParsedLines = parsedLines.filter(p => p.isRight).map(_.right.get)
+    return semanticalCheck(correctlyParsedLines)
+
+  }
+  def semanticalCheck(instructions: Array[Instruction]): Either[Array[ParseError], Array[Instruction]] = {
+
+    return Right(instructions)
+  }
+
+  def parseLine(line: Line): Either[ParseError, Instruction] = {
+
+    return null
+  }
 }
--- src/test/scala/vonemu/lang/TestLanguage.scala
+++ src/test/scala/vonemu/lang/TestLanguage.scala
@@ -3,9 +3,9 @@ package vonemu.lang
 
 import org.scalatest._
 import org.scalatest.FunSuite
-import vonemu.lang.Language._
+import vonemu.lang._
 
-
+    
 class LanguageSpec extends FlatSpec with Matchers{
 
   (2+2) should  equal (4)
@@ -14,11 +14,27 @@ class LanguageSpec extends FlatSpec with Matchers{
   (2+2) should be (4)
   (2+2) should not be 5
   
-  
-  
+    val add=BinaryOperation.ADD
+    val add2=BinaryOperation.ADD
+    val sub=BinaryOperation.SUB
+    val jmp=UnaryOperation.JMP
+    add should not be sub
+    add shouldBe add2
+    add should  not be jmp  
 }
 
+
 class LanguageSuite extends FunSuite {
-    val a = new AddRR(2,Register.AX,Register.BX)
+  
+//    val a = new Language.AddRR(2,Register.AX,Register.BX)
     test("one plus one is two")(assert(1 + 1 == 2))
+        val add=BinaryOperation.ADD
+     val x:Token=add
+  
+     val b=x match {
+      case x:Register => "Reg"
+      case x:BinaryOperation => "BOP"
+      case x:UnaryOperation => "UOP"
+    }
+    println(b)
 }
