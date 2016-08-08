c737e7b8fdd47ffd5b2543641246c10321d58d47
Facundo Quirogagit config --global user.email facundoq <facundoq@gmail.com>
2016-08-08 11:47:13 -0300

    Started implementing parser

--- src/test/scala/vonemu/lang/TestLanguage.scala
+++ src/test/scala/vonemu/lang/TestLanguage.scala
@@ -14,13 +14,7 @@ class LanguageSpec extends FlatSpec with Matchers{
   (2+2) should be (4)
   (2+2) should not be 5
   
-    val add=BinaryOperation.ADD
-    val add2=BinaryOperation.ADD
-    val sub=BinaryOperation.SUB
-    val jmp=UnaryOperation.JMP
-    add should not be sub
-    add shouldBe add2
-    add should  not be jmp  
+
 }
 
 
@@ -28,13 +22,5 @@ class LanguageSuite extends FunSuite {
   
 //    val a = new Language.AddRR(2,Register.AX,Register.BX)
     test("one plus one is two")(assert(1 + 1 == 2))
-        val add=BinaryOperation.ADD
-     val x:Token=add
   
-     val b=x match {
-      case x:Register => "Reg"
-      case x:BinaryOperation => "BOP"
-      case x:UnaryOperation => "UOP"
-    }
-    println(b)
 }
