eae7444c7d0ab736d33c1ce0c6da474d60eb4b61
Facundo Quiroga <facundoq@gmail.com>
2017-03-17 11:57:12 -0300

    implemented Compiler to perform the full parsing process

--- src/main/scala/vonemu/assembly/Compiler.scala
+++ src/main/scala/vonemu/assembly/Compiler.scala
@@ -3,7 +3,7 @@ package vonemu.assembly
 import vonemu.assembly.lexer.Lexer
 import vonemu.assembly.parser.Parser
 import vonemu.assembly.parser.Instruction
-
+import vonemu.assembly.lexer.EMPTY
 
 sealed trait CompilationError
 
@@ -14,14 +14,25 @@ case class Location(line: Int, column: Int) {
   override def toString = s"$line:$column"
 }
 
+object Compiler {
+  def apply(code: String): Either[List[CompilationError], List[Instruction]] = {
+    val instructions = code.split("\n")
+    var optionTokens = instructions map { Lexer(_) }
+    val fixedTokens = Lexer.fixLineNumbers(optionTokens)
 
-object WorkflowCompiler {
-  def apply(code: String): Either[CompilationError, Instruction] = {
-    for {
-      tokens <- Lexer(code).right
-      
-      ast <- Parser(tokens).right
-    } yield ast
-    
+    if (fixedTokens.count(_.isRight) == fixedTokens.length) {
+      val tokens = (fixedTokens map { _.right.get }) filter { t => !(t.length == 1 && t(0) == EMPTY()) }
+
+      val asts = tokens map { Parser(_) }
+      if (asts.count(_.isRight) == asts.length) {
+        Right(asts.map(v => v.right.get).toList)
+      }else{
+        Left(asts.filter(_.isLeft).map(v => v.left.get).toList)  
+      }
+    } else {
+      Left(fixedTokens.filter(_.isLeft).map(v => v.left.get).toList)
+    }
   }
 }
+  
+  
