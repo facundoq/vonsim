f25780b2d96b140c75cb2930c5eb7ea6e56e9628
Facundo Quiroga <facundoq@gmail.com>
2017-10-09 14:55:48 -0300

    updated code tutorial

--- src/main/scala/vonsim/webapp/tutorials/CodeTutorial.scala
+++ src/main/scala/vonsim/webapp/tutorials/CodeTutorial.scala
@@ -3,7 +3,7 @@ package vonsim.webapp.tutorials
 import vonsim.webapp.UIConfig
 
 class CodeTutorial extends Tutorial {
-  val title="Instrucciones de assembly"
+  val title="Instrucciones y registros de VonSim"
   
   val initialCode="""
 org 2000h
@@ -98,7 +98,7 @@ bx al registro dx.</p>
   
   <p>Algo que no se puede hacer es mover el valor de un registro hacia un valor</p>
   
-<p> Por ejemplo, la instrucción <code>mov 3, bx</code> es inválida.<p> 
+<p> Por ejemplo, la instrucción <code>mov 3, bx</code> es inválida.</p> 
 
 <p> O sea, en general, la sintaxis <code>mov valor, registro</code> es inválida.
 O sea, el orden de los operandos es importante</p>
@@ -107,9 +107,113 @@ O sea, el orden de los operandos es importante</p>
 
 """,UIConfig.enableAll,Some("org 2000h\nhlt\nend")
 )
+
+,TutorialStep("Parte alta y parte baja de un registro."
+       
+,"""<p> Los registros ocupan 2 bytes o 16 bits. Al byte más significativo se lo llama 
+<strong>parte alta</strong>, y al menos significativo, <strong>parte baja</strong></p>
+
+<p>Por ejemplo, si ax tiene el valor 3A4Fh, entonces el byte más significativo o parte alta
+vale 34h, y el byte menos significativo o parte baja vale 4Fh.</p>
+
+<p class="exercise"> Lee y prueba el código del editor. ¿Cómo queda almacenado el valor
+3A4Fh en el registro ax? ¿Cuál es la parte alta? ¿Y la baja?
+</p>
+
+<p class="answer">
+  La parte baja está en la primera fila: dicha fila está etiquetada con <strong>L</strong>, por
+  <strong>Low</strong> (bajo en inglés).
   
-,TutorialStep("Instrucción add (parte 1)"
-,"""<p>La instrucción add nos permite sumar dos números.</p>
+   La parte baja está en la segunda fila: dicha fila está etiquetada con <strong>H</strong>, por
+  <strong>High</strong> (alto en inglés).  
+</p>
+
+  
+""",UIConfig.enableAll,Some("org 2000h\nmov ax,3A4hh\nhlt\nend")
+)
+
+,TutorialStep("Modificando las partes altas y bajas de un registro."
+       
+,"""<p>En ocasiones, queremos manejar datos que sólo ocupan 8 bits, y no necesitamos
+toda la capacidad de un registro completo de 16 bits.</p>
+
+<p>En esos casos podemos acceder o cambiar sólo la parte alta o baja de los registros.</p>
+
+<p> Para cambiar el valor de un registro entero utilizamos los identificadores ax, bx, cx o dx.
+</p>
+
+<p> Pero también podemos cambiar sólo la parte alta o la parte baja utilizando otros identificadores.
+Para el registro ax, podemos utilizar los identificadores al y ah, para acceder a la parte 
+baja y a la parte alta de al directamente.
+</p>
+
+<p class="exercise">
+Lee y ejecuta el código del editor. ¿Cómo se modifica el registro ax?
+</p>
+
+<div class="answer">
+<p>
+El registro ax termina con el valor 5A94h, ya que en la parte alta (ah) se cargó el valor 5Ah,
+y en la parte baja (al) se cargó el valor 94h.
+</p>
+
+<p>
+De la misma forma, podemos acceder a las partes altas y bajas de bx con bl y bh, de cx con cl y ch
+y de dx con dl y dh.
+</p>
+</div>
+
+""",UIConfig.enableAll,Some("org 2000h\nmov ah,5Ah\nmov al,94h\nhlt\nend")
+)
+
+,TutorialStep("Independencia de los registros bajos y altos."
+       
+,"""<p class="exercise">
+  El código del editor tiene una instrucción que modifica todo el registro ax de una sola vez,
+  y otra que modifica solo su parte baja. Léelo y ejecútalo para ver cuáles
+  son los valores finales de los registros.
+  </p>
+
+<div class="answer"> 
+<p>
+El primer <code>mov</code> cambia el valor de todo el registro ax, tanto de 
+su parte alta como de su parte baja. Por otro lado, el segundo <code>mov</code> sólo
+cambia la parte baja. La parte alta sigue valiendo 5Ah, es decir, no se modificó.
+</p>
+<p>
+Por ende, podemos decir que cuando se accede directamente a la parte baja de ax utilizando
+el identificador al, en verdad estamos viendo a al como un registro independiente. Es decir,
+podríamos considerar que tenemos 4 registros de 16 bits (ax, bx, cx y dx) u 8 registros de 8 bits
+(al, ah, bl, bh, cl, ch, dl y dh) según nos convenga cuando escribimos el programa. 
+ </p>
+</div>
+
+""",UIConfig.enableAll,Some("org 2000h\nmov ax,5AC3h\nmov al,94h\nhlt\nend")
+)
+
+,TutorialStep("Modificando las partes altas y bajas de un registro."
+       
+,"""<p class="exercise">
+  Lee el código e intenta determinar cuales serán los valores finales de los registros
+  cuando termine el programa. Luego ejecuta el código y comprueba el resultado.
+  </p>
+
+""",UIConfig.enableAll,Some("""org 2000h
+mov bx,12
+mov bh,1Ah
+mov ah,5Ah
+mov al,94h
+mov cx,57h
+mov cl,al
+mov dl,12h
+mov dh,10
+mov dx,123h
+hlt
+end""")
+)
+
+,TutorialStep("Instrucción add (sumar)"
+,"""<p>Además de la instrucción <code>mov</code>, tenemos la instrucción <code>add</code> (<em>sumar</em> en inglés) nos permite sumar dos números.</p>
   
 <p> La sintaxis de dicha instrucción es <code>add registro, valor</code> <p>
 
@@ -125,7 +229,7 @@ ahora tendrá el valor 7. </p>
 """,UIConfig.enableAll,Some("org 2000h\nmov ax,4\nadd ax,3\nhlt\nend")
 )
 
-,TutorialStep("Instrucción add (parte 2)"
+,TutorialStep("Instrucción add con dos registros"
 ,"""<p>La instrucción add también nos permite sumar dos registros</p>
  <p> Entonces, <code>mov ax,bx</code> suma el valor de ax y el de bx<p> 
 <p>El resultado queda almacenado en ax.</p>
@@ -134,18 +238,140 @@ ahora tendrá el valor 7. </p>
 <p class="exercise">Agrega dos líneas al programa para darle el valor 2 a cx y luego sumarle cx a bx.
 El valor final del registro bx debería ser 5. </p>
 
-
 """,UIConfig.enableAll,Some("org 2000h\nmov ax,4\nmov bx,3\nadd ax,bx\nhlt\nend")
 )
 
-
-,TutorialStep("Instrucción add (parte 2)"
-,"""<p>La instrucción add también nos permite sumar dos registros</p>
- <p> Entonces, <code>mov ax,bx</code> suma el valor de ax y el de bx<p> 
-<p>El resultado queda almacenado en ax.</p>
-
-""",UIConfig.enableAll,Some("org 2000h\nmov ax,4\nadd ax,3\nadd ax,bx\nhlt\nend")
+,TutorialStep("Otras instrucciones aritméticas y lógicas"
+,"""<p>También hay otras instrucciones aritméticas y lógicas en el simulador.</p>
+<p>Las más comunes son <code>sub</code> (por <em>substract</em>, en inglés), para restar o sustraer,
+ y <code>or</code>, <code>xor</code> y <code>and</code> que hacen lo mismo que su equivalente lógico.
+<p> 
+<p class="exercise">
+Lee y ejecuta el código del editor, que utiliza instrucciones lógicas para calcular valores
+en los registros ax, bx, cx y dx. Observa el resultado.
+</p>
+<p class="answer">
+Las instrucciones <code>or</code>, <code>xor</code> y <code>and</code> aplican bit a bit las
+operaciones binarias del mismo nombre. La instrucción <code>sub</code> hace lo mismo que el 
+<code>add</code> pero restando. 
+</p>
+""",UIConfig.enableAll,Some("""org 2000h
+; sub, resta
+mov ax,4
+sub ax,3
+;or
+mov bl,11010010b
+or bl,10101010b
+;xor
+mov cl,11010010b
+xor cl,10101010b
+;and
+mov dl,11010010b
+and dl,10101010b
+hlt
+end""")
 )
+
+,TutorialStep("La instrucción <code>not</code>:."
+    
+    
+,"""<p> La instrucción <code>not</code> nos permite invertir el patrón de bits de un registro.</p>
+
+<p> Por ejemplo, si al vale 11011001b, aplicando el not pasa a valer 00100110b.
+</p>
+  
+<p>A diferencia de las otras instrucciones que vimos que son <em>binarias</em>, es decir,
+tienen dos operandos, el not es <em>unario<em>. Su sintaxis es simplemente <code>not registro</code>.</p>
+
+<p class="exercise"> Lee y ejecuta el código del editor. Verifica el valor final del registro al.</p>
+
+""",UIConfig.enableAll,Some("org 2000h\nmov al, 11011001b\nnot al\nhlt\nend")
+)
+
+,TutorialStep("La instrucción <code>neg</code>: multiplicar por -1."
+    
+,"""<p> La instrucción <code>neg</code> es similar al <code>not</code>, 
+  ya que también es unaria.</p>
+
+<p>
+Nos permite multiplicar a un número por menos uno, es decir, negarlo.
+</p>
+  
+
+<p class="exercise">
+Lee y ejecuta el código del editor. Verifica que los resultados finales sean correctos.
+Recuerda que los números negativos se almacenan codificados en CA2. 
+  </p>
+
+""",UIConfig.enableAll,Some("org 2000h\nmov bl,-4\nneg bl\nmov ch,7\nneg ch\nhlt\nend")
+)
+
+,TutorialStep("<code>inc</code> y <code>dec</code>" 
+    
+,"""<p> Cuando sumamos y restamos, algo muy común es que sumemos o restemos uno.</p>
+
+<p>
+Por ejemplo, si queremos sumarle 1 al registro ax y restarle 1 al registro bl, podemos escribir
+<code>add ax,1</code> y <code>sub bl,1</code>.
+</p>
+ 
+<p>
+Como estas operaciones de sumar y restar uno son 
+muy comunes, el simulador tiene instrucciones especiales para eso que se llaman <code>inc</code> 
+y <code>dec</code>. La primera incrementa un registro en uno y la otra lo decrementa
+</p>
+  
+<p>
+Lee y ejecuta el código del editor. Verifica que los resultados finales sean correctos. 
+</p> 
+
+<p class="exercise"> </p>
+
+""",UIConfig.enableAll,Some("org 2000h\nmov ax,5\ninc ax\nmov bl,20\ndec bl\nhlt\nend\n")
+)
+
+
+,TutorialStep("Un ejemplo completo TODO"
+    
+,"""<p> Los registros ocupan 2 bytes o 16 bits. Al byte más significativo se lo llama 
+<strong>parte alta</strong>, y al menos significativo, <strong>parte baja</strong></p>
+
+<p>
+
+</p>
+  
+<p> 
+</p> 
+
+
+<p class="exercise"> </p>
+
+""",UIConfig.enableAll,Some("org 2000h\nhlt\nend")
+)
+
+
+,TutorialStep("Resumen: TODO"
+    
+,"""<p> Los registros ocupan 2 bytes o 16 bits. Al byte más significativo se lo llama 
+<strong>parte alta</strong>, y al menos significativo, <strong>parte baja</strong></p>
+
+<p> Cada registro puede accederse o modificarse completo, utilizando, por ejemplo, 
+ax como identificador, o se pueden utilizar las partes altas y bajas de forma independiente,
+con ah y al como identificadores. Esto vale para todos los registros (ax, bx, cx y dx).
+</p>
+  
+<p>
+Hay varias instrucciones para modificar registros. Tenemos add, sub, inc y dec, para sumar y restar,
+or, xor, and y not, para realizar operaciones lógicas, y neg para hacer negativo (o positivo)
+ un número.
+</p> 
+
+
+
+
+""",UIConfig.enableAll,Some("org 2000h\nhlt\nend")
+)
+
 
 )
 
